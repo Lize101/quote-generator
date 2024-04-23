d1fd2ad9325a31def9b4f567237c9ad87114884f
Lizdigital <lizetteclaireh@gmail.com>
2024-04-23 17:10:37 +0100

    Add click event handler function, and event listener, to generate new quotes

--- app.js
+++ app.js
@@ -2,3 +2,11 @@ let quote = document.querySelector('.quote');
 let btn = document.querySelector('#newQuoteBtn');
 
 const allQuotes = ['You have got this', 'The best is yet to come', 'All in good time'];
+
+function generateNewQuote () {
+    let random = Math.floor(Math.random() * allQuotes.length); 
+
+    quote.innerText = allQuotes[random];
+}
+
+btn.addEventListener('click', generateNewQuote);
