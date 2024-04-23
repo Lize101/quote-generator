aa4e6fad26224d03bb2aebe4ca44c6cfc3df60d9
Lizdigital <lizetteclaireh@gmail.com>
2024-04-23 16:49:15 +0100

    Add variables and array for quotes

---
+++ app.js
@@ -0,0 +1,4 @@
+let quote = document.querySelector('.quote');
+let btn = document.querySelector('#newQuoteBtn');
+
+const allQuotes = ['You have got this', 'The best is yet to come', 'All in good time'];
